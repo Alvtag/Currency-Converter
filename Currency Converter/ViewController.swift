22b81a338ea3b007f91d8f8e79d7cc1a30bf38c0
Alvin Fong <aktfong@gmail.com>
2018-04-25 14:18:18 -0400

    outlets

--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var inputAmountLabel: UILabel!
+    @IBOutlet weak var outputAmountLabel: UILabel!
+    @IBOutlet weak var fromCurrencyPicker: UIPickerView!
+    @IBOutlet weak var toCurrencyPicker: UIPickerView!
+    @IBOutlet weak var infoTextLabel: UILabel!
+    
+    @IBAction func onInputButtonPrssed(_ sender: Any) {
+    }
+    @IBAction func onClearCacheButtonPressed(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+        
+    } 
 }
-
